19981885f4df3ddf4d67d4984b2fa301ccfd0d02
cmdax <cmdax@proton.me>
2024-05-14 10:56:56 +0200

    Set up the exercise

---
+++ DOM manipulation/script.js
@@ -0,0 +1,11 @@
+// <p> with red text saying 'Hey I'm red"
+
+// <h3> blue text 'I'm blue h3!'
+
+// <div>, black border, pink background
+
+    // <h1> 'I'm inside a div'
+    
+    // <p> 'ME TOO!'
+
+    
