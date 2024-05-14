56d01659d8330ba8bd714cf1aaeaaaa4fac2668e
cmdax <cmdax@proton.me>
2024-05-14 11:07:56 +0200

    Add the red <p> element

--- DOM manipulation/script.js
+++ DOM manipulation/script.js
@@ -1,5 +1,12 @@
-// <p> with red text saying 'Hey I'm red"
+// ADD EVERYTHING INSIDE THE CONTAINER DIV
+let container = document.querySelector('#container');
 
+// <p> with red text saying 'Hey I'm red"
+let pRed = document.createElement('p');
+pRed.style.color = 'red';
+pRed.textContent = "Hey, I'm red!";
+
+container.appendChild(pRed);
 // <h3> blue text 'I'm blue h3!'
 
 // <div>, black border, pink background
