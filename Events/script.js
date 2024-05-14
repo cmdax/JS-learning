068d1ae06e1d99b4db6a3a570b4578e8cd3e1ff7
cmdax <cmdax@proton.me>
2024-05-14 12:35:34 +0200

    Test creation of multiple eventListenener

--- Events/script.js
+++ Events/script.js
@@ -2,7 +2,12 @@ function alertFunction(){
     alert('You did it!');
 }
 
-const btn = document.querySelector("#btn");
-btn.addEventListener('click', function (e){
-    e.target.style.background = 'lightblue';
-});
+;
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert(button.id);
+    })
+})
+    
